b5756ca52103f14af276a14b94ae6f120716a3ad
Tony Kung <lps.tony@gmail.com>
2016-12-23 13:30:14 -0600

    and interpolation

---
+++ app.js
@@ -0,0 +1,9 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('mainController', ['$scope', '$timeout', function($scope, $timeout){
+  $scope.name = 'Tony';
+
+  $timeout(function() {
+    $scope.name = 'Everybody';
+  }, 3000);
+}]);
